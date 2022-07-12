c358449edbc039bc03a5cb5680e89a5cb57a23fc
Cristian Mihai <cristianmihai2696@gmail.com>
2022-07-12 15:36:01 +0300

    site drasky

---
+++ app.js
@@ -0,0 +1,64 @@
+//===================HEADER NAV=================//
+//====================BUTON1=====================//
+
+document.getElementById('buton1').onmouseover = function() {mouseOver1()};
+document.getElementById('buton1').onmouseout = function() {mouseOut1()};
+
+function mouseOver1() {
+    document.getElementById("navbtn1").style.backgroundPositionY= "0px";
+}
+
+function mouseOut1() {
+    document.getElementById("navbtn1").style.backgroundPositionY= "-80px";
+}
+
+//====================BUTON2=====================//
+
+document.getElementById('buton2').onmouseover = function() {mouseOver2()};
+document.getElementById('buton2').onmouseout = function() {mouseOut2()};
+
+function mouseOver2(){
+    document.getElementById('navbtn2').style.backgroundPositionY = "0px";
+}
+function mouseOut2(){
+    document.getElementById('navbtn2').style.backgroundPositionY = "-80px";
+}
+
+//====================BUTON3=====================//
+
+document.getElementById("buton3").onmouseover = function() {mouseOver()};
+document.getElementById("buton3").onmouseout = function() {mouseOut()};
+
+function mouseOver() {
+  document.getElementById("navbtn3").style.backgroundPositionY = "0px";
+}
+
+function mouseOut() {
+  document.getElementById("navbtn3").style.backgroundPositionY = "-80px";
+}
+
+//====================BUTON4=====================//
+
+document.getElementById('buton4').onmouseover = function() {mouseOver4()};
+document.getElementById('buton4').onmouseout = function() {mouseOut4()};
+
+function mouseOver4() {
+    document.getElementById('navbtn4').style.backgroundPositionY = "0px";
+}
+
+function mouseOut4(){
+    document.getElementById('navbtn4').style.backgroundPositionY= "-80px";
+}
+
+//====================BUTON5=====================//
+
+document.getElementById('buton5').onmouseover = function() {mouseOver5()};
+document.getElementById('buton5').onmouseout = function() {mouseOut5()};
+
+function mouseOver5(){
+    document.getElementById('navbtn5').style.backgroundPositionY = "0px";
+}
+
+function mouseOut5(){
+    document.getElementById('navbtn5').style.backgroundPositionY = "-80px";
+}
